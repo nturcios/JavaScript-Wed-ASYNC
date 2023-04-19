7416562ba447ed9a2fb9f9b93701a3f832203b16
Nelson Turcios <nelsonturcios124@yahoo.com>
2023-04-19 13:49:36 -0400

    Updated Commit

--- index.js
+++ index.js
@@ -12,3 +12,18 @@ function isDivisibleBy10(number) {
     return number % 10 === 0;
   }
 console.log(isDivisibleBy10(30)); //Result is True 
+
+//Return the number of vowels in a string
+
+function countVowels(str) {
+    const vowels = "aeiouAEIOU"
+    let count = 0;
+    for (let i = 0; i < str.length; i++){
+        if (vowels.includes(str[i])) {
+            count++;
+      }
+    }
+    return count;
+}
+console.log(countVowels("stay positive")); // Result is 5
+console.log(countVowels("Stay Consistent")) //Result is 4
